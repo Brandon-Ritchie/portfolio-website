fd9b03dcf3044e3d32bcfc82154f47e1a7e6a648
Brandon-Ritchie <britchie@brandonritchie.tech>
2021-11-19 19:38:12 -0500

    Added images for links on contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,17 +1,28 @@
 import React from "react";
+import "./Contact.css";
+import githubLogo from "../../images/github-logo.png";
+import emailIcon from "../../images/email-icon.png";
+import instagramLogo from "../../images/instagram-logo.png";
+import linkedInLogo from "../../images/linkedin-logo.png";
 
 export function Contact(props) {
   return (
     <div>
       <h2>Contact</h2>
       <p>Have something I can help you with? I would love to hear from you!</p>
-      <div>
-        <ul>
-          <li>Email</li>
-          <li>Instagram</li>
-          <li>Github</li>
-          <li>LinkedIn</li>
-        </ul>
+      <div className="socials">
+        <a href="mailto: britchie@brandonritchie.tech">
+          <img src={emailIcon} />
+        </a>
+        <a href="https://www.linkedin.com/in/brandon-ritchie-70106219a/">
+          <img src={linkedInLogo} />
+        </a>
+        <a href="https://www.instagram.com/_brandon.ritchie/">
+          <img src={instagramLogo} />
+        </a>
+        <a href="https://github.com/brandon-ritchie">
+          <img src={githubLogo} />
+        </a>
       </div>
     </div>
   );
