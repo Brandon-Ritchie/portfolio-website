cc9d28d004c5bbb1529b8bb923ddaa7e31e9175c
Brandon-Ritchie <ritchietrumpet@gmail.com>
2023-04-20 19:10:50 -0400

    fix: fix sass globals not working as expected

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script>
+    import '../global.scss'
+
+    const headerLinks = [
+        {
+            name: 'Home',
+            link: '/'
+        },
+        {
+            name: 'About',
+            link: '/about'
+        },
+        {
+            name: 'Contact',
+            link: '/contact'
+        },
+        {
+            name: 'Projects',
+            link: '/projects'
+        },
+        {
+            name: 'Blog',
+            link: '/blog'
+        }
+    ]
+</script>
+
+<div class="header">
+    <ul>
+        {#each headerLinks as link}
+            <li>
+                <a href={link.link}>{link.name}</a>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<slot/>
+
+<style lang="scss">
+
+</style>
