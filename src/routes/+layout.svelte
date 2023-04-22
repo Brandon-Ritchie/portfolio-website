e101377ea187d9b199f1fbd10b7ffaf993adaf05
Brandon-Ritchie <ritchietrumpet@gmail.com>
2023-04-22 11:30:05 -0400

    feat: base nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,42 @@
 <script>
-    import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
-    import '@skeletonlabs/skeleton/styles/all.css';
-    import '../app.postcss';
+	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton';
+	import 'iconify-icon';
+
+	const pages = [
+		{ name: 'Home', href: '/' },
+		{ name: 'About', href: '/about' },
+		{ name: 'Contact', href: '/contact' }
+	];
 </script>
 
-<slot/>
+<AppShell>
+	<svelte:fragment slot="pageHeader">
+		<AppBar slotTrail="place-content-end">
+			{#each pages as page}
+				<a href={page.href} class="font-bold px-2">{page.name}</a>
+			{/each}
+			<svelte:fragment slot="trail">
+				<a href="https://github.com/Brandon-Ritchie">
+					<iconify-icon icon="iconoir:github" />
+				</a>
+				<a href="https://twitter.com/ritchietrumpet">
+					<iconify-icon icon="iconoir:twitter" />
+				</a>
+				<a href="https://www.linkedin.com/in/brandon-ritchie-70106219a/">
+					<iconify-icon icon="iconoir:linkedin" />
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="pageFooter">
+		<AppBar slotTrail="place-content-end">
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+</AppShell>
